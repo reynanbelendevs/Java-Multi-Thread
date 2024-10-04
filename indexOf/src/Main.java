2f8d860b941cc99f3451ca4a4e35ef5c8e0dcaa3
reynan-belen <reynan_belen@trendmicro.com>
2024-10-04 15:16:51 +0800

    Laptop B

---
+++ indexOf/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String myStr = "Hello planet earth, you are a great planet.";
+        System.out.println(myStr.indexOf("planet",7));
+        }
+
+}
